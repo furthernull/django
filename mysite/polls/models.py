623c985a44f7a0c42519ebb83d54d211e6392506
Dmytro Sokolovskyi <dm.sokolovskyi@gmail.com>
2025-02-01 15:22:27 -0500

    cuctomize admin

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,6 +1,7 @@
 import datetime
 from random import choice
 
+from django.contrib import admin
 from django.db import models
 from django.utils import timezone
 
@@ -12,8 +13,14 @@ class Question(models.Model):
 	def __str__(self):
 		return f'{self.question_text} [{self.id}]'
 
+	@admin.display(
+		boolean=True,
+		ordering="pub_date",
+		description="Published recently?",
+	)
 	def was_published_recently(self):
-		return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+		now = timezone.now()
+		return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 
 class Choice(models.Model):
